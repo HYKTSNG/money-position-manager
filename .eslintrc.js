c7077fa7a77695b51d48bae0164f8eb57518821b
Shin <Sin5hayaK@gmail.com>
2020-08-06 17:52:24 +0900

    chore: Add ESLint settings & Prettier + Fix errors

---
+++ .eslintrc.js
@@ -0,0 +1,114 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  extends: [
+    "standard",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:jest/recommended",
+    /** @see https://github.com/prettier/eslint-config-prettier#installation */
+    "prettier/standard",
+    "prettier/@typescript-eslint",
+    "prettier/react",
+    "plugin:prettier/recommended",
+    "react-app",
+  ],
+  plugins: [
+    // CI で warn も検知可能にするため、全て error にする
+    "only-error",
+    "typescript-sort-keys",
+    "sort-keys-fix",
+  ],
+  rules: {
+    "import/no-default-export": "error",
+    "no-restricted-globals": [
+      "error",
+      {
+        name: "Date",
+        message:
+          "Do not call Date directly. Use `dateTimeUtils#getNow` instead.",
+      },
+    ],
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector: "TSEnumDeclaration",
+        message:
+          "Do not declare enums. Use `Plain Object` or `Literal Types` instead.",
+      },
+    ],
+    "prefer-template": "error",
+    "react/jsx-boolean-value": "error",
+    // jsx pragma に @emotion/core#jsx を使っている場合、Short Syntax 使えないため
+    "react/jsx-fragments": ["error", "element"],
+    "react/jsx-sort-props": "error",
+    "react/no-access-state-in-setstate": "error",
+    "react/no-array-index-key": "error",
+    "react/no-did-mount-set-state": "error",
+    "react/no-unsafe": ["error", { checkAliases: true }],
+    "react/prefer-stateless-function": "error",
+    "react/prop-types": "off",
+    "react/self-closing-comp": [
+      "error",
+      {
+        // empty な html が必要なケースはほとんどないため
+        html: false,
+      },
+    ],
+    "react/void-dom-elements-no-children": "error",
+    "sort-keys-fix/sort-keys-fix": "error",
+    "typescript-sort-keys/interface": "error",
+    "typescript-sort-keys/string-enum": "error",
+
+    // prettier
+    "prettier/prettier": [
+      "error",
+      {
+        arrowParens: "always",
+        semi: false,
+        trailingComma: "es5",
+      },
+    ],
+
+    // constructor のショートハンド（メンバーの省略記法）を許可
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "@typescript-eslint/no-parameter-properties": "off",
+
+    // React Component のボイラープレートコードを減らすため
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+      },
+    ],
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+
+    // バグの温床になりやすいコードを防ぐため
+    "@typescript-eslint/no-unused-expressions": [
+      "error",
+      {
+        allowShortCircuit: false,
+        allowTernary: false,
+        allowTaggedTemplates: false,
+      },
+    ],
+
+    // 有用なケースがあるため
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+
+    // Other
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-misused-promises": "error",
+    "@typescript-eslint/prefer-readonly": "error",
+
+    // note you must disable the base rule as it can report incorrect errors
+    "require-await": "off",
+    "@typescript-eslint/require-await": "error",
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "src/logo.svg";
 import "src/App.css";
 
-function App() {
+export const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +21,4 @@ function App() {
       </header>
     </div>
   );
-}
-
-export default App;
+};
