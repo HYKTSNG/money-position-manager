580daa4793a91af0331b6f043fca89223d93a20d
Shin <Sin5hayaK@gmail.com>
2020-08-11 01:11:25 +0900

    chore: Install emotion & normalize.css &robots.txt

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from "react";
-import logo from "src/logo.svg";
-import "src/App.css";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core"
+
+import React from "react"
+import logo from "src/logo.svg"
 
 export const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div css={root}>
+      <header css={headerCss}>
+        <img alt="logo" css={logoCss} src={logo} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          css={link}
           href="https://reactjs.org"
-          target="_blank"
           rel="noopener noreferrer"
+          target="_blank"
         >
           Learn React
         </a>
       </header>
     </div>
-  );
-};
+  )
+}
+
+const root = css`
+  text-align: center;
+`
+
+const headerCss = css`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+
+const logoCss = css`
+  height: 40vmin;
+  pointer-events: none;
+
+  animation: App-logo-spin infinite 20s linear;
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`
+
+const link = css`
+  color: #61dafb;
+`
