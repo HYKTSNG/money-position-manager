dbbd9815919b84ada1d1c90046cb5c249dbf98a7
Shin <Sin5hayaK@gmail.com>
2020-08-26 21:09:02 +0900

    fix: delete unused test code

--- src/components/pages/index/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { Index } from "./"
-import React from "react"
-import { render } from "@testing-library/react"
-
-test("renders learn react link", () => {
-  const { getByText } = render(<Index />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
-})
