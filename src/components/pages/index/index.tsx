5f1be78cc9fd3d929cb96e1afca28ef2dd61575a
Shin <Sin5hayaK@gmail.com>
2020-08-26 21:00:30 +0900

    fix:リファクタリング

--- src/components/pages/index/index.tsx
+++ src/components/pages/index/index.tsx
@@ -1,18 +1,87 @@
+import React, { useState } from "react"
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core"
 
-import React from "react"
+import { TextField } from "@material-ui/core"
+
+type Props = {
+  children: never
+}
 
 export const Index: React.FC = () => {
+  const [enterIncome, setEnterIncome] = useState(0)
+  const [enterExpense, setEnterExpense] = useState(0)
+
+  const [incomeCount, setIncomeCount] = useState(0)
+  const [expenseCount, setExpenseCount] = useState(0)
+
+  const onClickIncome = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void => {
+    // 収入の計算
+    e.preventDefault()
+    setEnterIncome(enterIncome + Number(incomeCount))
+  }
+
+  const onClickExpense = (e: { preventDefault: () => void }): void => {
+    // 出費の計算
+    e.preventDefault()
+    setEnterExpense(enterExpense - Number(expenseCount))
+  }
+
+  const sumMoney = enterIncome - enterExpense
+
   return (
     <div css={root}>
-      <p>
-        Edit <code>src/App.tsx</code>and save to reload.
-      </p>
+      <p css={subTitle}>現在の貯金</p>
+      <div>{sumMoney}</div>
+
+      <form autoComplete="off" noValidate>
+        {/* 収入 */}
+        <p css={subTitle}>収入</p>
+        <div>{enterIncome}</div>
+        <TextField
+          id="standard-basic"
+          label="income"
+          onChange={(e) => setIncomeCount(Number(e.target.value))}
+          type="number"
+        />
+        <button css={btn} onClick={onClickIncome}>
+          ADD
+        </button>
+      </form>
+
+      <form autoComplete="off" noValidate>
+        {/* 出費 */}
+        <p css={subTitle}>出費</p>
+        <div>{expenseCount}</div>
+        <TextField
+          id="standard-basic"
+          label="expense"
+          onChange={(e) => setExpenseCount(Number(e.target.value))}
+          type="number"
+        />
+        <button css={btn} onClick={onClickExpense}>
+          ADD
+        </button>
+      </form>
     </div>
   )
 }
 
 const root = css`
   text-align: center;
+  margin: 80px;
+  font-size: 32px;
+  width: 25;
+`
+const subTitle = css`
+  margin: 32px;
+  font-size: 64px;
+  background-color: palegreen;
+`
+
+const btn = css`
+  background-color: gold;
+  border-color: yellow;
 `
