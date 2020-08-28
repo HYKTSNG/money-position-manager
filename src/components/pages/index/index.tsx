6b8285fe3b9403fab4dc48cb5a6fcdbf7a9af2da
Shin <Sin5hayaK@gmail.com>
2020-08-28 16:36:13 +0900

    feat: 理由付けたし

--- src/components/pages/index/index.tsx
+++ src/components/pages/index/index.tsx
@@ -4,27 +4,49 @@ import { TextField } from "@material-ui/core"
 import React, { useState } from "react"
 
 export const Index: React.FC = () => {
-  const [enterIncome, setEnterIncome] = useState(0)
-  const [enterExpense, setEnterExpense] = useState(0)
+  const [enterNumIncome, setEnterIncome] = useState(0)
+  const [enterNumExpense, setEnterExpense] = useState(0)
 
-  const [incomeCount, setIncomeCount] = useState(0)
-  const [expenseCount, setExpenseCount] = useState(0)
+  const [incomeSum, setIncomeCount] = useState(0)
+  const [expenseSum, setExpenseCount] = useState(0)
 
-  const onClickIncome = (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  const [enterCharIncome, setEnterCharIncome] = useState("")
+  const [enterCharExpense, setEnterCharExpense] = useState("")
+
+  const [reasonIncome, setReasonIncome] = useState("")
+  const [reasonExpense, setReasonExpense] = useState("")
+
+  const onClickNumIncome = (
+    eventNum: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ): void => {
     // 収入の計算
-    e.preventDefault()
-    setEnterIncome(enterIncome + Number(incomeCount))
+    eventNum.preventDefault()
+    setEnterIncome(enterNumIncome + Number(incomeSum))
+  }
+
+  const onClickCharIncome = (
+    eventChar: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void => {
+    // 収入の理由
+    eventChar.preventDefault()
+    setEnterCharIncome(reasonIncome)
   }
 
   const onClickExpense = (e: { preventDefault: () => void }): void => {
     // 出費の計算
     e.preventDefault()
-    setEnterExpense(enterExpense + Number(expenseCount))
+    setEnterExpense(enterNumExpense + Number(expenseSum))
   }
 
-  const sumMoney = enterIncome - enterExpense
+  const onClickCharExpense = (
+    eventChar: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void => {
+    // 収入の理由
+    eventChar.preventDefault()
+    setEnterCharExpense(reasonExpense)
+  }
+
+  const sumMoney = enterNumIncome - enterNumExpense
 
   return (
     <div css={root}>
@@ -33,30 +55,58 @@ export const Index: React.FC = () => {
       <p css={subTitle}>現在の貯金</p>
       <div>{sumMoney}</div>
 
-      <form autoComplete="off" noValidate>
-        {/* 収入 */}
+      {/* 収入 */}
+      <form autoComplete="off" css={text} noValidate>
         <p css={subTitle}>収入</p>
         <div>収入額</div>
-        <div>{enterIncome}</div>
+        <div>
+          {enterCharIncome}
+          <span>
+            から<span>{enterNumIncome}</span>円入金されました
+          </span>
+        </div>
+
+        <TextField
+          id="standard-basic"
+          label="Reason"
+          onChange={(e) => setReasonIncome(e.target.value)}
+        />
+        <button css={btn} onClick={onClickCharIncome}>
+          ADD
+        </button>
         <TextField
           id="standard-basic"
           label="income"
           onChange={(e) => setIncomeCount(Number(e.target.value))}
           type="number"
         />
-        <button css={btn} onClick={onClickIncome}>
+        <button css={btn} onClick={onClickNumIncome}>
           ADD
         </button>
       </form>
 
-      <form autoComplete="off" noValidate>
-        {/* 出費 */}
+      {/* 出費 */}
+      <form autoComplete="off" css={text} noValidate>
         <p css={subTitle}>出費</p>
         <div>出費額</div>
         <div>
-          <span>-</span>
-          {enterExpense}
+          {enterCharExpense}
+          <span>
+            から<span>{enterNumExpense}</span>円出金されました
+          </span>
         </div>
+        <div>
+          <span>-</span>
+          {enterNumExpense}
+        </div>
+        <TextField
+          id="standard-basic"
+          label="Reason"
+          onChange={(e) => setReasonExpense(e.target.value)}
+        />
+        <button css={btn} onClick={onClickCharExpense}>
+          ADD
+        </button>
         <TextField
           id="standard-basic"
           label="expense"
@@ -87,8 +137,11 @@ const subTitle = css`
   font-size: 48px;
   background-color: palegreen;
 `
+const text = css`
+  letter-spacing: 8px;
+`
 
 const btn = css`
-  background-color: gold;
-  border-color: yellow;
+  color: #fff;
+  background-color: chartreuse;
 `
